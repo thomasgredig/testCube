7c6b6c9fed87b0f2a8cf9d696f6a7829a1fa8f56
Thomas Gredig <thomas.gredig@csulb.edu>
2019-04-20 15:05:39 -0700

    R package Cube
    cube and square functions for R package sample

---
+++ R/Cubing.R
@@ -0,0 +1,21 @@
+#' Square numbers
+#'
+#' @param x A number.
+#' @return The Square.
+#' @examples
+#' squared(5)
+squared <- function(x) {
+  x*x
+}
+
+#' Cube numbers
+#'
+#' @param x A number.
+#' @return The Cube
+#' @examples
+#' cubed(5)
+cubed <- function(x) {
+  x*x*x
+}
+
+
---
+++ tests/testthat.R
@@ -0,0 +1,5 @@
+library(testthat)
+library(testCube)
+
+cubed(3) == 27
+squared(-5) == 25
